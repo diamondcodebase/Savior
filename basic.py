5a35cf4004ede63d1da7334aee506496bfe93e30
Diamond Dai <diamond.dai60@gmail.com>
2024-06-14 16:22:31 -0600

    align centre style and handle database update and query

--- basic.py
+++ basic.py
@@ -1,24 +1,28 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
-from datetime import timedelta
+from datetime import datetime, timedelta
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = "hello" # secret_key is essential to start the session
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.sqlite3" # configure the sqlite database setting
 app.config["SQLALCHEMY_TRACK_MODIFICATION"] = False # this is optional setting
-app.permanent_session_lifetime = timedelta(minutes=5) # set the permanent
+
+allowTime = 2
+app.permanent_session_lifetime = timedelta(minutes=allowTime) # set the permanent
 
 db = SQLAlchemy(app) # set up the database
 
-# define a user class to define the data model to database table user
-class users(db.Model):
+# define a hero class to define the data model to database table hero
+class heros(db.Model):
     _id = db.Column("id", db.Integer, primary_key=True) # unique identification
     name = db.Column(db.String(100)) # if you dont define name, the parameter name is same as variable
-    email = db.Column(db.String(200))
+    password = db.Column(db.String(200))
+    modifyTime = db.Column(db.String(200))
 
-    def __init__(self, name, email):
+    def __init__(self, name, password, modifyTime):
         self.name = name
-        self.email = email
+        self.password = password
+        self.modifyTime = modifyTime
 
 
 @app.route("/")
@@ -34,10 +38,21 @@ def register():
         # if the request is POST, store the hero and password in session
         hero = request.form["hero"]
         password = request.form["password"]
-        session["hero"] = hero
-        session["password"] = password
-        # redirect to the view page with session of hero and password
-        return redirect(url_for("view"))
+
+        found_hero = heros.query.filter_by(name=hero).first() # query the selected hero from database
+        if found_hero:
+            # update database record
+            found_hero.password = password
+            found_hero.modifyTime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            db.session.commit()
+
+            session["hero"] = hero
+            session["password"] = password
+
+            # redirect to the view page with session of hero and password
+            return redirect(url_for("view"))
+        else:
+            return render_template("register.html")
     else:
         return render_template("register.html")
 
@@ -86,13 +101,18 @@ def level3():
         hero = request.form["hero"]
         password = request.form["password"]
 
+        if not session["hero"] or not session["password"]:
+            flash("Your hero power time is over, mission failed. Please return to the starting point")
         if hero != session["hero"]:
             flash("Your hero name is incorrect, please correct and try again")
         elif password != session["password"]:
             flash("Your password is incorrect, please correct and try again")
         else:
-            return redirect(url_for("congrat")) # if player give correct information of hero and password, go to congrat, end game
-    
+            found_hero = heros.query.filter_by(name=hero).first() # query the selected hero from database
+            if datetime.strptime(found_hero.modifyTime, '%Y-%m-%d %H:%M:%S') + timedelta(0, 60*allowTime) < datetime.now():
+                flash("Your hero power time is over, mission failed. Please return to the starting point")
+            else:
+                return redirect(url_for("congrat")) # if player give correct information of hero and password, go to congrat, end game    
     return render_template("level3.html")
 
 
